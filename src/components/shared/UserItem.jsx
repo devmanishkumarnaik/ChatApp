1179bf3df5c92d3b1fdda47ee570b35764444410
Manish Kumar Naik <165175167+devmanishkumarnaik@users.noreply.github.com>
2025-02-18 17:15:23 +0530

    Update UserItem.jsx

--- src/components/shared/UserItem.jsx
+++ src/components/shared/UserItem.jsx
@@ -2,7 +2,7 @@ import { Avatar, IconButton, ListItem, Stack, Typography } from "@mui/material";
 import React, { memo } from "react";
 import {Add as AddIcon, Remove as RemoveIcon} from "@mui/icons-material";
 import {transformImage} from "../../lib/features";
-import checkpic from "../../../public/check.png";
+import checkpic from "/check.png";
 
 const UserItem = ({ user, handler, handlerIsLoading, isAdded = false, styling = {}}) => {
   const { name, username, _id, avatar} = user;
